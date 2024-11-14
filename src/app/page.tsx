4bd7b061b61c39cde1c7d7d2f48eec6bc57bdc1a
Shah Jabir <shahjabirtaqi@gmail.com>
2024-11-14 18:27:14 +0600

    feature: hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { FaHome } from 'react-icons/fa';
+
+import Hero from './../components/Hero';
+import { FloatingNav } from './../components/ui/FloatingNav';
+
 export default function Home() {
   return (
     <>
-      <main>
-        <h1>Hello, World!</h1>
+      <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
+        <FloatingNav
+          navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }]}
+        />
+        <Hero />
       </main>
     </>
   );
